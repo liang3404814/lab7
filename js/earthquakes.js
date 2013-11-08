0054a3ffdadcef9de6e5865e930789da732ab3c9
liang3404814 <liang3404814@gmail.com>
2013-11-08 13:59:51 -0800

    fixed infoWindow for function scope

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -3,7 +3,7 @@
 
     SODA data source URL: https://soda.demo.socrata.com/resource/earthquakes.json
     app token (pass as '$$app_token' query string param): Hwu90cjqyFghuAWQgannew7Oi
-*/
+    */
 
 //create a global variable namespace based on usgs.gov
 //this is how JavaScript developers keep global variables
@@ -27,6 +27,58 @@ gov.usgs.quakesMap;
 //AJAX Error event handler
 //just alerts the user of the error
 $(document).ajaxError(function(event, jqXHR, err){
-    alert('Problem obtaining data: ' + jqXHR.statusText);
+	alert('Problem obtaining data: ' + jqXHR.statusText);
 });
 
+$(function() {
+	getQuakes();
+});
+
+function getQuakes() {
+	$.getJSON(gov.usgs.quakesUrl, function(quakes) {
+
+		gov.usgs.quakesMap = new google.maps.Map($('.map-container')[0], {
+			center: new google.maps.LatLng(0, 0),
+			zoom: 2,
+			mapTypeId: google.maps.MapTypeId.TERRAIN,
+			streetViewControl: false
+		});
+
+		gov.usgs.quakes = quakes;
+		
+		$('.message').html("Displaying " + quakes.length + " earthquakes");
+		addQuakeMarkers(quakes, gov.usgs.quakesMap);
+	});
+}
+
+function addQuakeMarkers(quakes, map) {
+	var quake;
+	var idx;
+
+	for (idx = 0; idx < quakes.length; ++idx) {
+		quake = quakes[idx];
+
+		if (quake.location.longitude && quake.location.latitude) {
+			var lat = quake.location.latitude;
+			var lng = quake.location.longitude;
+			quake.mapMarker = new google.maps.Marker({
+				map: map,
+				position: new google.maps.LatLng(lat, lng)
+			});
+
+			addInfoWindowForQuake(quake, map);
+		}
+	}
+}
+
+function addInfoWindowForQuake(quake, map) {
+	google.maps.event.addListener(quake.mapMarker, 'click', function() {
+		gov.usgs.iw = new google.maps.InfoWindow({
+			content: new Date(quake.datetime).toLocaleString() +
+			': magnitude ' + quake.magnitude + ' at depth of ' +
+			quake.depth + ' meters'
+		});
+
+		gov.usgs.iw.open(map, this);
+	});
+}
